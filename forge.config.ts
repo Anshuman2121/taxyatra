f194c5b110ddb48dfe017805c7d52dfbc95b63b4
Nitesh Ranjan <niteshranjan@Niteshs-MacBook-Air.local>
2025-11-19 15:50:46 +0530

    Revert "updated code to support postgres db"
    This reverts commit 578cf02735a434412cac48fb3e1c7ea93d060878.

--- forge.config.ts
+++ forge.config.ts
@@ -22,7 +22,7 @@ const config: ForgeConfig = {
     new VitePlugin({
       build: [
         {
-          entry: 'src/backend/main.ts',
+          entry: 'src/main.ts',
           config: 'vite.main.config.ts',
           target: 'main',
         },
